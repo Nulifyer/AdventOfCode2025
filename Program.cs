71fd9c80bb7ab11484c8ac75782ec40201820d0e
Nulifyer <kyle.edward.leddy@gmail.com>
2025-12-02 18:01:43 -0500

    part 2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿
 var lines = File.ReadAllLines(@"./input.txt");
 
-int num_zeros = 0;
-var dial = new Dial(99,50);
+var dial = new Dial(99, 50);
 
 Console.WriteLine($"The dial starts by pointing at {dial.Position}.");
 
@@ -20,38 +19,80 @@ foreach (var line in lines)
             dial.RotateRight(num);
             break;
     }
-
-    Console.WriteLine($"The dial is rotated {line} to point at {dial.Position}.");
-
-    if (dial.Position == 0) num_zeros++;
 }
 
-;
+Console.WriteLine($"The dial pointed at zero {dial.NumZeros} times.");
 
 public class Dial
 {
+    public int NumZeros { get; private set; }
     public readonly int TotalTicks;
     public int Position { get; private set; }
 
     public Dial(int total_ticks = 99, int position = 0)
     {
+        NumZeros = 0;
         TotalTicks = total_ticks + 1;
         Position = position;
     }
 
     public void RotateLeft(int ticks)
     {
-        ticks = ticks % TotalTicks;
+        int num_full_rotaions = ticks / TotalTicks;
+        if (num_full_rotaions > 0)
+        {
+            NumZeros += num_full_rotaions;
+            Console.WriteLine($"\tThe dial is rotated left fully {num_full_rotaions} times.");
+        }
+
+        ticks = ticks % TotalTicks; // get real ticks
+
+        if (Position > 0 && Position - ticks < 0)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated left past zero 1 times.");
+        }
+
         Position = Position - ticks;
         if (Position < 0)
             Position = TotalTicks + Position;
+
+        if (Position == 0)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated left to zero 1 times.");
+        }
+
+        Console.WriteLine($"The dial is now at {Position}. -{ticks}");
     }
 
     public void RotateRight(int ticks)
     {
-        ticks = ticks % TotalTicks;
+        int num_full_rotaions = ticks / TotalTicks;
+        if (num_full_rotaions > 0)
+        {
+            NumZeros += num_full_rotaions;
+            Console.WriteLine($"\tThe dial is rotated right fully {num_full_rotaions} times.");
+        }
+
+        ticks = ticks % TotalTicks; // get real ticks
+
+        if (Position != 0 && Position + ticks > TotalTicks)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated right past zero 1 times.");
+        }
+
         Position = Position + ticks;
         if (Position >= TotalTicks)
             Position = Position - TotalTicks;
+
+        if (Position == 0)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated right to zero 1 times.");
+        }
+
+        Console.WriteLine($"The dial is now at {Position}. +{ticks}");
     }
 }
