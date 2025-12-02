c555e8a4453deb0623e4fccce2800e1e92772923
Nulifyer <kyle.edward.leddy@gmail.com>
2025-12-02 18:10:30 -0500

    init

---
+++ 1/Program.cs
@@ -0,0 +1,98 @@
+﻿
+var lines = File.ReadAllLines(@"./input.txt");
+
+var dial = new Dial(99, 50);
+
+Console.WriteLine($"The dial starts by pointing at {dial.Position}.");
+
+foreach (var line in lines)
+{
+    char dir = line[0];
+    int num = Convert.ToInt32(line.Substring(1));
+
+    switch (dir)
+    {
+        case 'L':
+            dial.RotateLeft(num);
+            break;
+        case 'R':
+            dial.RotateRight(num);
+            break;
+    }
+}
+
+Console.WriteLine($"The dial pointed at zero {dial.NumZeros} times.");
+
+public class Dial
+{
+    public int NumZeros { get; private set; }
+    public readonly int TotalTicks;
+    public int Position { get; private set; }
+
+    public Dial(int total_ticks = 99, int position = 0)
+    {
+        NumZeros = 0;
+        TotalTicks = total_ticks + 1;
+        Position = position;
+    }
+
+    public void RotateLeft(int ticks)
+    {
+        int num_full_rotaions = ticks / TotalTicks;
+        if (num_full_rotaions > 0)
+        {
+            NumZeros += num_full_rotaions;
+            Console.WriteLine($"\tThe dial is rotated left fully {num_full_rotaions} times.");
+        }
+
+        ticks = ticks % TotalTicks; // get real ticks
+
+        if (Position > 0 && Position - ticks < 0)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated left past zero 1 times.");
+        }
+
+        Position = Position - ticks;
+        if (Position < 0)
+            Position = TotalTicks + Position;
+
+        if (Position == 0)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated left to zero 1 times.");
+        }
+
+        Console.WriteLine($"The dial is now at {Position}. -{ticks}");
+    }
+
+    public void RotateRight(int ticks)
+    {
+        int num_full_rotaions = ticks / TotalTicks;
+        if (num_full_rotaions > 0)
+        {
+            NumZeros += num_full_rotaions;
+            Console.WriteLine($"\tThe dial is rotated right fully {num_full_rotaions} times.");
+        }
+
+        ticks = ticks % TotalTicks; // get real ticks
+
+        if (Position != 0 && Position + ticks > TotalTicks)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated right past zero 1 times.");
+        }
+
+        Position = Position + ticks;
+        if (Position >= TotalTicks)
+            Position = Position - TotalTicks;
+
+        if (Position == 0)
+        {
+            ++NumZeros;
+            Console.WriteLine($"\tThe dial is rotated right to zero 1 times.");
+        }
+
+        Console.WriteLine($"The dial is now at {Position}. +{ticks}");
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Transactions;
+
+var raw = File.ReadAllText(@"./input.txt");
+
+ulong sum_invalid_ids = 0;
+
+var ranges = raw
+    .Split(',')
+    .Select(x => x.Trim())
+    .Where(x => x.Length > 0 && x.All(x => "0123456789-".Contains(x)))
+    .Select(x => x.Split('-'))
+    .Where(x => x.Length == 2)
+    .Select(x => new
+    {
+        Low = Convert.ToUInt64(x[0]),
+        High = Convert.ToUInt64(x[1]),
+    })
+    .ToArray();
+
+foreach (var range in ranges)
+{
+    for (ulong i = range.Low; i <= range.High; ++i)
+    {
+        string i_str = i.ToString();
+        if (HasRepeatChunk(i_str))
+        {
+            Console.WriteLine($"{range.Low}-{range.High} has invalid ID, {i_str}");
+            sum_invalid_ids = sum_invalid_ids + i;
+            continue;
+        }
+    }
+}
+
+Console.WriteLine($"Sum: {sum_invalid_ids}");
+
+;
+
+bool HasRepeatChunk(string num_str)
+{
+    string? last_chunk, chunk;
+
+    int saw_same_chunk = 0;
+    int expected_repeat; 
+
+    for (int sub_len = 1; sub_len <= num_str.Length / 2; ++sub_len)
+    {
+        last_chunk = null;
+        saw_same_chunk = 1;
+        
+        expected_repeat = num_str.Length / sub_len;
+        if (sub_len * expected_repeat != num_str.Length) continue; // if not divided equally, skip
+
+        for (int i = 0; i + sub_len <= num_str.Length; i += sub_len)
+        {
+            chunk = num_str.Substring(i, sub_len);
+
+            if (last_chunk == null)
+            {
+                last_chunk = chunk;
+                continue;
+            }
+            else if (last_chunk == chunk)
+            {
+                ++saw_same_chunk;
+
+                if (saw_same_chunk == expected_repeat)
+                    return true;
+            }
+            else
+            {
+                break; // non matching chunk size
+            }
+        }
+    }
+
+    return false;
+}
