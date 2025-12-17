4ef09da834aa8ac497c4a7df0400c3252acf4950
Nulifyer <kyle.edward.leddy@gmail.com>
2025-12-17 12:18:19 -0500

    read part 4-1

---
+++ 4/Program.cs
@@ -0,0 +1,36 @@
+﻿var lines = File.ReadLines(@"./example.txt");
+
+int line_len = 0;
+var paper = new List<bool>();
+
+foreach (var line in lines)
+{
+    if (line_len == 0) line_len = line.Length;
+    paper.AddRange(line.ToCharArray().Select(x => x == '@'));
+}
+
+var paper_arr = paper.ToArray();
+paper.Clear();
+paper = null;
+
+PrintPaper();
+
+;
+
+
+
+
+
+
+
+
+void PrintPaper()
+{
+    for (int i = 0; i < paper_arr.Length; i++)
+    {
+        if (i > 0 && i % line_len == 0)
+            Console.Write('\n');
+        
+        Console.Write(paper_arr[i] ? '@' : '.');
+    }
+}
